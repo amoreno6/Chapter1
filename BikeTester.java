fa698008145fc38d7dd21b7db3fac6286ae131b7
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-09-13 12:35:17 -0400

    Update BikeTester.java

--- BikeTester.java
+++ BikeTester.java
@@ -2,7 +2,7 @@
 * Author: Adrian Moreno
 * Course: ITEC 2150
 * 9/13/2022
-* This class – Prints out all details found in the bike class
+* This class – Tests methods from bike class as well as places both bikes into an array.
 */
 
 import java.util.ArrayList;
@@ -13,19 +13,23 @@ public class BikeTester
        
         Bike bike1 = new Bike();
         System.out.println(bike1.toString());
+        //Prints out the bike1 details with the toString method
         
         Bike bike2 = new Bike();
         bike2.setNumOfWheels(4);
         bike2.setManufacturer("Mongoose");
         bike2.SetYear(2010);
         System.out.println(bike2);
+        //New bike with details set and printed
         
         ArrayList<Object> bikeList = new ArrayList<>();
         bikeList.add(bike1);
         bikeList.add(bike2);
+        //Creates the array named ArrayList
+        //Both bikes added into  ArrayList
        
         System.out.println(bikeList);
-
+        //Prints out all bikes in the array
     }
 
 }
